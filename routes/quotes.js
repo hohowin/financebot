c597505e681c684c21e68716208b94ec1afb7a78
Howin Ho <ho1ho@yahoo.com>
2022-07-06 17:49:09 -0400

    use then

--- routes/quotes.js
+++ routes/quotes.js
@@ -31,40 +31,37 @@ quotesRouter.get('/stock', async (req, res) => {
 
 		try {
 
-			await Promise.all([
+			await axios.get('https://yh-finance.p.rapidapi.com/market/v2/get-quotes?region=US&symbols=' + symbol, {
+				headers: {
+					'X-RapidAPI-Key': TOKEN,
+					'X-RapidAPI-Host': 'yh-finance.p.rapidapi.com'
+				}
+			}).then(
+				(val) => {
+					name = val.data.quoteResponse.result[0].longName;
+					price = val.data.quoteResponse.result[0].regularMarketPrice;
+					currency = val.data.quoteResponse.result[0].currency;
 
-				axios.get('https://yh-finance.p.rapidapi.com/market/v2/get-quotes?region=US&symbols=' + symbol, {
-					headers: {
-						'X-RapidAPI-Key': TOKEN,
-						'X-RapidAPI-Host': 'yh-finance.p.rapidapi.com'
-					}
-				}).then(
-					(val) => {
-						name = val.data.quoteResponse.result[0].longName;
-						price = val.data.quoteResponse.result[0].regularMarketPrice;
-						currency = val.data.quoteResponse.result[0].currency;
+					hash['Beta (5Y Monthly)'] = _.get(val, 'data.quoteResponse.result[0].beta', 'N/A');
+					hash['EPS Current Year'] = _.get(val, 'data.quoteResponse.result[0].epsCurrentYear', 'N/A');
+					hash['EPS (TTM)'] = _.get(val, 'data.quoteResponse.result[0].epsTrailingTwelveMonths', 'N/A');
+					hash['Forward PE'] = _.get(val, 'data.quoteResponse.result[0].forwardPE', 'N/A');
+					hash['PE Ratio (TTM)'] = _.get(val, 'data.quoteResponse.result[0].trailingPE', 'N/A');
+				}
+			);
 
-						hash['Beta (5Y Monthly)'] = _.get(val, 'data.quoteResponse.result[0].beta', 'N/A');
-						hash['EPS Current Year'] = _.get(val, 'data.quoteResponse.result[0].epsCurrentYear', 'N/A');
-						hash['EPS (TTM)'] = _.get(val, 'data.quoteResponse.result[0].epsTrailingTwelveMonths', 'N/A');
-						hash['Forward PE'] = _.get(val, 'data.quoteResponse.result[0].forwardPE', 'N/A');
-						hash['PE Ratio (TTM)'] = _.get(val, 'data.quoteResponse.result[0].trailingPE', 'N/A');
-					}
-				),
-
-				axios.get('https://yh-finance.p.rapidapi.com/stock/v3/get-statistics?symbol=' + symbol, {
-					headers: {
-						'X-RapidAPI-Key': TOKEN,
-						'X-RapidAPI-Host': 'yh-finance.p.rapidapi.com'
-					}
-				}).then(
-					(val) => {
-						hash['Trailing P/E'] = _.get(val, 'data.timeSeries.trailingPeRatio[0].reportedValue.fmt', 'N/A');
-						hash['Forward P/E (TTM)'] = _.get(val, 'data.timeSeries.trailingForwardPeRatio[0].reportedValue.fmt', 'N/A');
-						hash['PEG Ratio (5 yr expected)'] = _.get(val, 'data.timeSeries.trailingPegRatio[0].reportedValue.fmt', 'N/A');
-					}
-				)
-			]);
+			await axios.get('https://yh-finance.p.rapidapi.com/stock/v3/get-statistics?symbol=' + symbol, {
+				headers: {
+					'X-RapidAPI-Key': TOKEN,
+					'X-RapidAPI-Host': 'yh-finance.p.rapidapi.com'
+				}
+			}).then(
+				(val) => {
+					hash['Trailing P/E'] = _.get(val, 'data.timeSeries.trailingPeRatio[0].reportedValue.fmt', 'N/A');
+					hash['Forward P/E (TTM)'] = _.get(val, 'data.timeSeries.trailingForwardPeRatio[0].reportedValue.fmt', 'N/A');
+					hash['PEG Ratio (5 yr expected)'] = _.get(val, 'data.timeSeries.trailingPegRatio[0].reportedValue.fmt', 'N/A');
+				}
+			);
 
 			console.log(JSON.stringify(hash, null, 2));
 
